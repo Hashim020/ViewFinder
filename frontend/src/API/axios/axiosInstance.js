2d42a4fb2b2211f404573052b76a6086c79d7c5e
Hashim M A <ma.hashim200@gmail.com>
2024-04-11 14:53:04 +0530

    base Url

--- frontend/src/API/axios/axiosInstance.js
+++ frontend/src/API/axios/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = "http://localhost:5000";
+const baseURL = "http://3.110.35.122:5000";
 const instance = axios.create({
   baseURL: baseURL,
   timeout: 10000,
