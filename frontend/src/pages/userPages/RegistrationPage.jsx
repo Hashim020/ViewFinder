abff6511deb8808bf4f45be7a478146e7ecb1d0a
Hashim M A <ma.hashim200@gmail.com>
2024-04-16 10:49:50 +0530

    bug fix

--- frontend/src/pages/userPages/RegistrationPage.jsx
+++ frontend/src/pages/userPages/RegistrationPage.jsx
@@ -36,7 +36,7 @@ const RegistrationPage = () => {
     }
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!emailRegex.test(email)) {
-      setError("Enter a Valid Email");
+      toast.error("Enter a Valid Email");
       return
     }
     onOpen();
