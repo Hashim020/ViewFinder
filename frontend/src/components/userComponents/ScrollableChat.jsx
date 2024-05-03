9b40e2a16725e31015acc04b4ff5379d75ec0c15
Hashim M A <ma.hashim200@gmail.com>
2024-05-03 14:36:43 +0530

    fix

--- frontend/src/components/userComponents/ScrollableChat.jsx
+++ frontend/src/components/userComponents/ScrollableChat.jsx
@@ -18,53 +18,45 @@ const ScrollableChat = ({ messages }) => {
   }, [messages]);
 
   return (
-    <div
-    ref={scrollRef}
-    style={{
-      height: "510px",
-      marginTop: "10px",
-      overflowY: "auto",
-    }}
-  >
-    {messages &&
-      messages.map((message, index) => (
-        <div
-          key={message._id}
-          style={{
-            display: "flex",
-            justifyContent: message.sender._id === user._id ? "flex-end" : "flex-start",
-          }}
-        >
-          {(isSameSender(messages, message, index, user._id) ||
-            isLastMessage(messages, index, user._id)) && (
-            <Tooltip label={message.sender.name} placement="bottom-start" hasArrow>
-              <Avatar
-                mt="7px"
-                mr={1}
-                size="sm"
-                cursor="pointer"
-                name={message.sender.name}
-                src={message.sender.profileImageName}
-              />
-            </Tooltip>
-          )}
-          <span
+    <div ref={scrollRef} style={{ height: "510px", marginTop: "10px", overflowY: "auto" }}>
+      {messages &&
+        messages.map((m, i) => (
+          <div
             style={{
-              backgroundColor: `${
-                message.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
-              }`,
-              marginLeft: isSameSenderMargin(messages, message, index, user._id) ? 0 : 10,
-              marginTop: isSameUser(messages, message, index, user._id) ? 3 : 10,
-              borderRadius: "20px",
-              padding: "5px 15px",
-              maxWidth: "75%",
+              display: "flex",
+              justifyContent: m.sender._id === user._id ? "flex-end" : "flex-start",
             }}
+            key={m._id}
           >
-            {message.content}
-          </span>
-        </div>
-      ))}
-  </div>
+            {(isSameSender(messages, m, i, user._id) ||
+              isLastMessage(messages, i, user._id)) && (
+                <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
+                  <Avatar
+                    mt="7px"
+                    mr={1}
+                    size="sm"
+                    cursor="pointer"
+                    name={m.sender.name}
+                    src={m.sender.profileImageName}
+                  />
+                </Tooltip>
+              )}
+            <span
+              style={{
+                backgroundColor: `${m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
+                  }`,
+                marginLeft: isSameSenderMargin(messages, m, i, user._id) ? 0 : 10,
+                marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
+                borderRadius: "20px",
+                padding: "5px 15px",
+                maxWidth: "75%",
+              }}
+            >
+              {m.content}
+            </span>
+          </div>
+        ))}
+    </div>
   );
 };
 
